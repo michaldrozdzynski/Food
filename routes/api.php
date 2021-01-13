fe47c343ddfdf11314dc61bb84888c22ab64c186
Michał Drożdżyński <michaldrozdzynski99@gmail.com>
2021-01-13 22:19:22 +0100

    fixed auth

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\AuthBasic;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,26 +15,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/foodporn', 'FoodpornController@index');
-Route::post('/foodporn', 'FoodpornController@store');
-Route::patch('/foodporn/{id}/good', 'FoodpornController@good');
-Route::patch('/foodporn/{id}/bad', 'FoodpornController@bad');
-Route::delete('/foodporn/{id}', 'FoodpornController@destroy');
+Route::middleware([AuthBasic::class])->group(function () {
+    Route::get('/foodporn', 'FoodpornController@index');
+    Route::post('/foodporn', 'FoodpornController@store');
+    Route::patch('/foodporn/{id}/good', 'FoodpornController@good');
+    Route::patch('/foodporn/{id}/bad', 'FoodpornController@bad');
+    Route::delete('/foodporn/{id}', 'FoodpornController@destroy');
 
-Route::get('/foodrecipe', 'FoodRecipeController@index');
-Route::post('/foodrecipe', 'FoodRecipeController@store');
-Route::get('/foodrecipe/{id}', 'FoodRecipeController@show');
-Route::patch('/foodrecipe/{id}/good', 'FoodRecipeController@good');
-Route::patch('/foodrecipe/{id}/bad', 'FoodRecipeController@bad');
-Route::put('/foodrecipe/{id}', 'FoodRecipeController@update');
-Route::delete('/foodrecipe/{recipe}', 'FoodRecipeController@destroy');
+    Route::get('/foodrecipe', 'FoodRecipeController@index');
+    Route::post('/foodrecipe', 'FoodRecipeController@store');
+    Route::get('/foodrecipe/{id}', 'FoodRecipeController@show');
+    Route::patch('/foodrecipe/{id}/good', 'FoodRecipeController@good');
+    Route::patch('/foodrecipe/{id}/bad', 'FoodRecipeController@bad');
+    Route::put('/foodrecipe/{id}', 'FoodRecipeController@update');
+    Route::delete('/foodrecipe/{recipe}', 'FoodRecipeController@destroy');
 
-Route::get('/comment/{recipeId}', 'CommentController@index');
-Route::post('/comment', 'CommentController@store');
-Route::patch('/comment/{comment}/good', 'CommentController@good');
-Route::patch('/comment/{comment}/bad', 'CommentController@bad');
+    Route::get('/comment/{recipeId}', 'CommentController@index');
+    Route::post('/comment', 'CommentController@store');
+    Route::patch('/comment/{comment}/good', 'CommentController@good');
+    Route::patch('/comment/{comment}/bad', 'CommentController@bad');
 
-Route::get('/conversation', 'ConversationController@index');
-Route::post('/conversation/{user}', 'ConversationController@store');
-Route::get('/conversation/{conversation}', 'ConversationController@show');
-Route::post('/conversation/{conversation}/send', 'ConversationController@send');
+    Route::get('/conversation', 'ConversationController@index');
+    Route::post('/conversation/{user}', 'ConversationController@store');
+    Route::get('/conversation/{conversation}', 'ConversationController@show');
+    Route::post('/conversation/{conversation}/send', 'ConversationController@send');
+});
